0f6dc8d11c69793701f35c4010606f1423ad82bb
Sowmiya1921051 <sowmiyanallasivam3434@gmail.com>
2024-11-29 17:42:20 +0530

    modification

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],  // Correct preset
-  plugins: ['nativewind/babel'], // NativeWind Babel plugin
+  presets: ['module:@react-native/babel-preset'],
+  plugins: ["nativewind/babel"],
 };
---
+++ src/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const App = () => {
+  return (
+    <View className="flex-1 justify-center items-center bg-gray-100">
+      <Text className="text-blue-500 font-bold text-2xl">Hello, NativeWind!</Text>
+    </View>
+  );
+};
+
+export default App;
