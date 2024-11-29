d986b9b393e62543f528e37b2abc3c9600512671
Sowmiya1921051 <sowmiyanallasivam3434@gmail.com>
2024-11-29 16:34:52 +0530

    modification

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React from 'react';
+import { Text, View } from 'react-native';
 
 const App = () => {
   return (
-    <View>
-      <Text className="font-bold text-2xl text-blue-500">App</Text>
+    <View className="flex-1 justify-center items-center bg-gray-100">
+      <Text className="text-blue-500 font-bold text-2xl">Hello, NativeWind!</Text>
     </View>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
- content: [
-  "./App.{js,jsx,ts,tsx}",
-   "./src/**/*.{js,jsx,ts,tsx}",
-   "./screen/**/*.{js,jsx,ts,tsx}",
+  content: [
+    "./App.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",  // Add other custom folder paths if needed
+    "./screen/**/*.{js,jsx,ts,tsx}",  // Add additional directories as necessary
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
